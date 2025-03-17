a7dd541452e9cb8d42a14d600c8f4b5869cae90e
excniesNIED <hxcn@cnies.org>
2025-03-17 16:44:55 +0800

    Update site configuration in config.ts with new details for BrushUP

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,18 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://bu.cnies.org/", // replace this with your deployed domain
+  author: "Churnie HXCN",
+  profile: "https://hxcn.cnies.org/",
+  desc: "萑澈的错题本",
+  title: "BrushUP",
+  ogImage: "https://gastigado.cnies.org/d/cover/8ff1b88f90de4620f718a.jpg?sign=LybqQcH9n66GJfwEaovM6BYHOEXdcP-cWjWLlxf8xPA=:0",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
+  postPerIndex: 10,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    url: "https://github.com/satnaing/astro-paper/edit/main/src/content/blog",
+    url: "https://github.com/excniesNIED/HXCN_Astro_BrushUp/edit/main/src/content/blog",
     text: "Suggest Changes",
     appendFilePath: true,
   },
