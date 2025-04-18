26b3c6e7969f6ffe98d5ae86049a892d2b4f6c5f
excniesNIED <hxcn@cnies.org>
2025-04-19 01:58:30 +0800

    remove exp config

--- astro.config.ts
+++ astro.config.ts
@@ -37,10 +37,5 @@ export default defineConfig({
   image: {
     // Used for all Markdown images; not configurable per-image
     // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
-    experimentalLayout: "responsive",
-  },
-  experimental: {
-    svg: true,
-    responsiveImages: true,
   },
 });
