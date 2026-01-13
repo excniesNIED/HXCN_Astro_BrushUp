f643ab82ecfc8b75c4c4086ad80c95115ed78dc3
excniesNIED <hxcn@cnies.org>
2026-01-13 10:26:24 +0800

    chore: normalize line endings from CRLF to LF
    - Convert astro.config.ts from CRLF to LF line endings
    - Convert src/constants.ts from CRLF to LF line endings
    - Convert src/layouts/PostDetails.astro from CRLF to LF line endings
    - Remove WhatsApp, Facebook, and Pinterest from SHARE_LINKS array
    - Remove unused icon imports (IconWhatsapp, IconFacebook, IconPinterest)

--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,7 @@ import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
 import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
-import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
-import IconPinterest from "@/assets/icons/IconPinterest.svg";
 
 import { SITE } from "@/config";
 import IconBrandQq from "@/assets/icons/IconBrandQq.svg";
@@ -48,18 +45,6 @@ export const SOCIALS: Social[] = [
 
 export const SHARE_LINKS: Social[] = [
   {
-    name: "WhatsApp",
-    href: "https://wa.me/?text=",
-    linkTitle: `Share this post via WhatsApp`,
-    icon: IconWhatsapp,
-  },
-  {
-    name: "Facebook",
-    href: "https://www.facebook.com/sharer.php?u=",
-    linkTitle: `Share this post on Facebook`,
-    icon: IconFacebook,
-  },
-  {
     name: "X",
     href: "https://x.com/intent/post?url=",
     linkTitle: `Share this post on X`,
@@ -70,12 +55,6 @@ export const SHARE_LINKS: Social[] = [
     href: "https://t.me/share/url?url=",
     linkTitle: `Share this post via Telegram`,
     icon: IconTelegram,
-  },
-  {
-    name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
-    linkTitle: `Share this post on Pinterest`,
-    icon: IconPinterest,
   },
   {
     name: "Mail",
